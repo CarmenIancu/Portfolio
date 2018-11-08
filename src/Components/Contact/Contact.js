5b0df214f4fe5884ee44ac55d30c62cf7afdc89d
CarmenIancu <carmen.iancu.mail@gmail.com>
2018-11-08 11:27:22 +0200

    fixing contact and footer appearance on small screen

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -4,10 +4,12 @@ import about from '../../images/about.jpg';
 
 const Contact = () => {
 	return (
-		<div id='contact' className='f4 tc dark-gray grow inline-flex items-center ma2 tc br2 pa2'>
-			<img className='br-pill fl' height={150} width={150} src={about} alt='carmen iancu'/>
-			<IoIosMail color={'#fdb803'} size={30}/>
-		  <a className='no-underline' href='mailto:carmen.iancu.mail@gmail.com'>carmen.iancu.mail@gmail.com</a>
+		<div id='contact' className='f4 dark-gray items-center grow pa3 br2'>
+			<img className='br-pill fc' height={100} width={100} src={about} alt='carmen iancu'/>
+			<div className='inline-flex'>
+				<IoIosMail color={'#fdb803'} size={30}/>
+		  		<a className='no-underline' href='mailto:carmen.iancu.mail@gmail.com'>carmen.iancu.mail@gmail.com</a>
+			</div>
 		</div>
 		)
 }
--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css';
 
 const Footer = () => {
 	return (
-<footer className='near-white bg-animate bg-near-black hover-bg-gray items-center pa2'>
+<footer className='near-white bg-animate bg-near-black hover-bg-gray items-center pa2 w-100'>
   <small className='f6 db tc'>© 2018 Made with <span className='heart'></span> by <b className='ttu'> Carmen Iancu </b></small>
 </footer>
 		)
