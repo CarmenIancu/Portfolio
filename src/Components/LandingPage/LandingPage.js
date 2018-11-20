ed93adf03c35b9a00c6fe93ff680d256b4e018ce
CarmenIancu <42655525+CarmenIancu@users.noreply.github.com>
2018-11-20 11:34:20 +0200

    Update LandingPage.js

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -9,8 +9,8 @@ const LandingPage =() => {
   <div className='vh-100 dt w-100 tc bg-dark-gray white cover no-repeat center'>
     <div className='dtc v-mid dark-gray'> 
     	<Navbar  />
-       	<h1 className="f-headline lh-solid">Carmen Iancu</h1>
-	<h1 className="f1 lh-title">front-end developer</h1>
+       	<h1 className="f1 lh-solid">Carmen Iancu</h1>
+	<h1 className="f2 lh-title">front-end developer</h1>
         <Links />
     </div>
    </div>
